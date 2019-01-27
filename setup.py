8993e7fa1f54eda0d8d9d377596c59759967ce47
Victor "Nate" Graf <nategraf1@gmail.com>
2019-01-27 10:50:40 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+setuptools.setup(
+    name='netcapture',
+    description='Network capture and manipulation module',
+    packages=['netcapture'],
+    version='0.1.0',
+    url='https://github.com/nategraf/netcapture',
+
+    author='Victor "Nate" Graf',
+    author_email="nategraf1@gmail.com",
+    license='MIT',
+
+    long_description=open("README.rst", "r").read(),
+    long_description_content_type="text/x-rst",
+    keywords=[],
+
+    install_requires=['scapy'],
+    python_requires='>=3',
+
+    classifiers=[
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+
+	'Topic :: Security',
+	'Topic :: Communications',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
