a8ed38b9423e1a895f7371e29208d8ae63e4099f
Victor "Nate" Graf <nategraf1@gmail.com>
2019-01-31 21:25:52 -0600

    add StopSniffing exception

--- snare/sniffer.py
+++ snare/sniffer.py
@@ -5,6 +5,9 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+class StopSniffing(Exception):
+    """StopSniffing is raised while processing a packet to indicate stop the sniffer."""
+
 class Sniffer:
     """
     Sniffer is the core component of the traffic capture framework.
@@ -27,54 +30,73 @@ class Sniffer:
         self._stopevent = threading.Event()
         self._moduleslock = threading.RLock()
         self._newmodules = []
+        self._activemodules = []
 
     def register(self, *mods):
+        """Add new modules to the sniffer"""
         with self._moduleslock:
             self.modules.extend(mods)
             self._newmodules.extend(mods)
 
     def process(self, pkt):
+        """Process the given packet through each active module, and self.processor"""
         with self._moduleslock:
-            for mod in self.modules:
-                if mod not in self._newmodules:
+            for mod in self._activemodules:
+                try:
                     mod.process(pkt)
+                except StopSniffing:
+                    self._stopevent.set()
+
         if self.processor is not None:
-            self.processor(pkt)
+            try:
+                self.processor(pkt)
+            except StopSniffing:
+                self._stopevent.set()
 
     def run(self):
+        """Run the sniffer on the current thread, blocking until it terminates"""
         try:
             self._l2socket = scapy.conf.L2listen(iface=self.iface, filter=self.filter)
 
             while not self._stopevent.is_set():
+                # Start any newly added modules.
                 with self._moduleslock:
                     while self._newmodules:
-                        self._newmodules.pop().start(self)
+                        mod = self._newmodules.pop()
+                        mod.start(self)
+                        self._activemodules.append(mod)
 
+                # Sniff for one quantum, processing packets as we go.
                 pkts = self._l2socket.sniff(timeout=self.quantum, prn=self.process, store=self.store)
                 self.packets.extend(pkts)
         finally:
+            # Stop all the active modules and close the sniffing socket.
             with self._moduleslock:
-                for mod in self.modules:
-                    mod.stop()
+                while self._activemodules:
+                    self._activemodules.pop().stop()
 
             if self._l2socket is not None:
                 self._l2socket.close()
                 self._l2socket = None
 
     def start(self):
+        """Start the sniffer on a new thread"""
         self._stopevent.clear()
         if self._thread is None or not self._thread.is_alive():
             with self._moduleslock:
                 self._newmodules = list(self.modules)
+                self._activemodules = list()
             self._thread = threading.Thread(target=self.run, daemon=True)
             self._thread.start()
 
+    def stop(self):
+        """Signal the sniffer to stop terminate"""
+        self._stopevent.set()
+
     def join(self):
+        """Block until the sniffer thread has terminated"""
         if self._thread is not None:
             self._thread.join()
-
-    def stop(self):
-        self._stopevent.set()
 
     def __enter__(self):
         self.start()
@@ -85,24 +107,25 @@ class Sniffer:
 
 class Module:
     """
-    Module is the base for a packet sniffer module.
-    Implementaions of Module provide a discrete functionality towards complex packet analysis and manipulation.
+    Module provides a feature on top of the sniffing platform.
+    User defined modules should inherit from this class.
     """
     def start(self, sniffer):
         """
-        Start will be called when the sniffer starts
+        Start when the sniffer starts or this module is added to a running sniffer.
         """
         pass
 
     def process(self, pkt):
         """
-        Process will be called for every packet recieved by the sniffer
+        Process will be called for every packet recieved by the sniffer.
+        Process may raise StopSniffing to signal that the sniffer should terminate.
         """
         pass
 
     def stop(self):
         """
-        Stop will be called when the sniffer stops
+        Stop will be called when the sniffer stops.
         """
         pass
 
