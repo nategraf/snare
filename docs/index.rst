eaa08343a2df5c8199b23099b3c9a6e02084919b
Victor Graf <nategraf1@gmail.com>
2020-09-06 20:52:50 +0000

    add sphinx docs directory and dostring to snare.net.addr

---
+++ docs/index.rst
@@ -0,0 +1,77 @@
+.. snare documentation master file, created by
+   sphinx-quickstart on Sun Sep  6 19:28:49 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to snare's documentation!
+=================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+
+.. include:: ../README.rst
+
+Sniffer
+-------
+
+.. automodule:: snare.sniffer
+   :members:
+
+ARP
+---
+
+.. automodule:: snare.arp
+   :members:
+
+TCP
+---
+
+.. automodule:: snare.tcp
+   :members:
+
+Forward
+-------
+
+.. automodule:: snare.forward
+   :members:
+
+Net
+---
+
+.. automodule:: snare.net
+   :members:
+
+Addr
+~~~~
+
+.. automodule:: snare.net.addr
+   :members:
+
+iptables
+~~~~~~~~
+
+.. automodule:: snare.net.iptables
+   :members:
+
+OpenVPN
+~~~~~~~
+
+.. automodule:: snare.net.openvpn
+   :members:
+
+Route
+~~~~~
+
+.. automodule:: snare.net.route
+   :members:
--- snare/net/addr.py
+++ snare/net/addr.py
@@ -19,9 +19,15 @@ class AddrMeta(type):
         return (0x1 << (cls.bytelen * 8)) - 1
 
 class Addr(metaclass=AddrMeta):
+    """Generic network address with support for various conversions and operators"""
     bytelen = 0
 
     def __init__(self, addr):
+        """Create an address from string, int, or bytes
+
+        Args:
+            addr (str|int|bytes|Addr): Address to create in string, int or bytes representation.
+        """
         self._str = None
         self._int = None
         self._bytes = None
@@ -82,6 +88,7 @@ class Addr(metaclass=AddrMeta):
         return '<{0}.{1} {2!s}>'.format(__name__, type(self).__name__, self)
 
 class Ip(Addr):
+    """IPv4 address with support for conversion to/from strings and byte operations"""
     bytelen = 4
 
     @staticmethod
@@ -103,23 +110,32 @@ class Ip(Addr):
         return 32 - i
 
 class IpNet:
+    """IPv4 address and network mask, representing a subnet."""
     def __init__(self, ip, mask):
         self.ip = Ip(ip)
         self.mask = Ip(mask)
 
     def network(self):
+        """Calculate the network address from the IP and mask."""
         return self.ip & self.mask
 
     def broadcast(self):
+        """Calculate the broadcast address from the IP and mask."""
         return self.ip | ~self.mask
 
     def contains(self, ip):
+        """Determines if the given address is in this subnet
+
+        Args:
+            ip (Ip): IPv4 to check if contained in this subnet.
+        """
         return (Ip(ip) & self.mask) == self.network()
 
     def __contains__(self, ip):
         return self.contains(ip)
 
     def cidr(self):
+        """Returns the CIDR representation of this subnet."""
         return "{!s}/{:d}".format(self.ip, self.mask.slash())
 
     def __str__(self):
@@ -129,6 +145,7 @@ class IpNet:
         return '<{0}.{1} {2!s}>'.format(__name__, type(self).__name__, self)
 
 class Mac(Addr):
+    """Ethernet MAC address"""
     bytelen = 6
 
     @staticmethod
@@ -150,18 +167,63 @@ def _ifctl(ifname, code):
     )
 
 def ifaddr(ifname):
+    """Retrieves the IPv4 address for the given network interface name.
+
+    Args:
+        ifname (str): Interface name to retreive the IP address from.
+
+    Examples:
+        >>> ifaddr('lo')
+        <snare.net.addr.Ip 127.0.0.1>
+    """
     return Ip(_ifctl(ifname, 0x8915)[20:24]) # SIOCGIFADDR
 
 def ifmask(ifname):
+    """Retrieves the IPv4 network mask for the given network interface name.
+
+    Args:
+        ifname (str): Interface name to retreive the IP network mask from.
+
+    Examples:
+        >>> ifmask('lo')
+        <snare.net.addr.Ip 255.0.0.0>
+    """
     return Ip(_ifctl(ifname, 0x891b)[20:24]) # SIOCGIFNETMASK
 
 def ifhwaddr(ifname):
+    """Retrieves the Ethernet MAC address for the given network interface name.
+
+    Args:
+        ifname (str): Interface name to retreive the MAC address from.
+
+    Examples:
+        >>> ifhwaddr('lo')
+        <snare.net.addr.Mac 00:00:00:00:00:00>
+    """
     return Mac(_ifctl(ifname, 0x8927)[18:24]) # SIOCGIFHWADDR
 
 def parsecidr(ipnet):
+    """Parses and returns the given CIDR string as an IpNet subnet object.
+
+    Args:
+        ipnet (str): String representation of the subnet.
+
+    Examples:
+        >>> parsecidr('172.16.224.70/20')
+        <snare.net.addr.IpNet 172.16.224.70/20>
+    """
     ipstr, maskstr = ipnet.split('/')
     maskint = 0xffffffff ^ ((0x00000001 << (32-int(maskstr)))-1)
     return IpNet(ipstr, maskint)
 
 def ifcidr(ifname):
+    """Retrieves the IPv4 address and network mask from the given inerface name.
+
+    Args:
+        ifname (str): Interface name to retreive the IP address and network mask from.
+
+    Examples:
+        >>> ifcidr('lo')
+        <snare.net.addr.IpNet 127.0.0.1/8>
+    """
     return IpNet(ifaddr(ifname), ifmask(ifname))
